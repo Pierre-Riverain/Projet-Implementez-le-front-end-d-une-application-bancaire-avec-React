c2a28d53d9a616107a6cf0c3fb5a9f1c6cec7d35
Pierre Riverain <pierre_riverain@hotmail.fr>
2024-03-19 15:44:38 +0100

    => Création du composant Header. => Création du composant Footer. => Création du store. => Modification du Composant Général App afin d'appliquer la mise en page globale des pages du site.

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,7 +1,17 @@
+import { Provider } from 'react-redux';
 import "./main.css";
+import { store } from './Store';
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
+    <Provider store={store}>
+      <Header />
+
+      <Footer />
+    </Provider>
   );
 }
 
---
+++ frontend/src/app/Store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+let state = {
+
+};
+
+export const store = configureStore({
+    preloadedState: state,
+    reducer: combineReducers({
+
+    });
+})
